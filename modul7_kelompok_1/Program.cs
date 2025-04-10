a35da79f3015efb5a7e47040744632070a3172c5
Haafzd <haafizd.alhabib@gmail.com>
2025-04-10 11:14:20 +0700

    menambahkan class TeamMembers103022330089

--- modul7_kelompok_1/Program.cs
+++ modul7_kelompok_1/Program.cs
@@ -1,9 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-class Program
+﻿class Program
 {
     public static void Main(string[] args)
     {
-        DataMahasiswa103022330089 kuliah = new DataMahasiswa103022330089();
-        kuliah.readJSON();
+        DataMahasiswa103022330089 data = new DataMahasiswa103022330089();
+        TeamMembers103022330089 anggota = new TeamMembers103022330089();
+        anggota.ReadJSON();
+        data.readJSON();
     }
 }
---
+++ modul7_kelompok_1/TeamMembers103022330089.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class TeamMembers103022330089
+{
+    public class Member
+    {
+        public string firstName;
+        public string lastName;
+        public string gender;
+        public int age;
+        public string nim;
+    }
+
+    public class MemberList
+    {
+        public List<Member> members;
+    }
+
+    public void ReadJSON()
+    {
+        string dataJson = File.ReadAllText("../../../jurnal7_2_103022330089.json");
+
+         var id = JsonSerializer.Deserialize<MemberList>(dataJson, new JsonSerializerOptions { IncludeFields = true });
+
+        Console.WriteLine("Team Member List:");
+        int i = 1;
+        foreach (Member m in id.members)
+        {
+            Console.WriteLine($"{m.nim} - {m.firstName} {m.lastName} ({m.age} - {m.gender})");
+        }
+    }
+}
